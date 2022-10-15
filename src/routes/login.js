ecf46c9fd2294a33d878df67a1f18bad1156417a
Nicolas <yncampos@ucundinamarca.edu.co>
2022-10-15 16:51:46 -0500

    Ajustes

--- src/routes/login.js
+++ src/routes/login.js
@@ -94,22 +94,18 @@ router.post('/login', async (req, res) => {
         } else {
             var sql = `SELECT * FROM usuario WHERE correo = "${correo}"`;
             conexion.query(sql, (error, results) => {
-                if (results.length == 0 && contraseña == results[0].contraseña ) {
-
+                if (results.length == 0 || (contraseña != results[0].contraseña) ) {
+                    res.status(400).send("Credenciales Incorrectas");
                 } else {
                     //console.log(results);
+                    console.log(results[0].contraseña);
                     const id = results[0].idUsuario;
                     
-                    const token = jwt.sign({ id: id }, "loginSecret");
-                    res.json({ status: 'ok', message: 'Bienvenido', token });
-                    console.log("TOKEN: " + token + " para el Usuario: " + id);
-                    /*
-                    const cookiesOptions = {
-                        expires: new Date(Date.now() + settings.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-                        httpOnly: true
-                    }
-                    res.cookie('jwt', token, cookiesOptions);
-                    */
+                    const token = jwt.sign({ id: id, email: correo }, "loginSecret", {expiresIn: "2h"});
+                    let nDatos = {...results[0], token};
+                    res.status(200).json(nDatos);
+                    //res.json({ status: 'ok', message: 'Bienvenido', token });
+                    //console.log("TOKEN: " + token + " para el Usuario: " + id);
                 }
 
             })
