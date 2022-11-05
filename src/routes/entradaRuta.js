2fbc8f06e29bc8db0094cd0ff44e5ae5d83b7af4
Nicolas <yncampos@ucundinamarca.edu.co>
2022-11-05 12:52:59 -0500

    AjustesAPI entrada

--- src/routes/entradaRuta.js
+++ src/routes/entradaRuta.js
@@ -4,6 +4,8 @@ const conexion = require('../connection/conexion');
 
 //------------------------------------------------------------ Obtener entradas ----------------------------------------------------------------
 
+
+
 router.get('/entrada', async (req, res) => {
     try {
         var sql = "SELECT * FROM entrada WHERE estado = 'Activo'"
@@ -44,6 +46,55 @@ router.post('/entrada', async (req, res) => {
         console.log(error);
     }
 })
+
+//------------------------------------------------------------ Consultar entrada ----------------------------------------------------------------
+
+router.get('/entrada/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        if (!isNaN(id)){
+            var sql = `SELECT * FROM entrada WHERE idEntrada = ${id}`;
+            conexion.query(sql, (err, results) => {
+                if(!err){
+                    res.status(200).json(results)
+                }else{
+                    res.status(400).send('ERROR')
+                }                
+            })
+        }else{
+            res.status(500).json({ "msg": "Debe ser un numero" });
+        }
+    }catch(error){
+        console.log(error);
+    }
+})
+
+//------------------------------------------------------------ Consultar entrada ----------------------------------------------------------------
+router.put('/entrada/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        if(!isNaN(id)){
+            var sql = `UPDATE entrada SET`
+            + ` idProducto = ${req.body.idProducto}`
+            + `, idProveedor = ${req.body.idProveedor}`
+            + `, montoTotal = ${req.body.montoTotal}`
+            + `, estado = '${req.body.estado}'`
+            + ` WHERE idEntrada = ${id}`
+            conexion.query(sql, (err, result) => {
+                if(!err){
+                    res.status(200).json(result)
+                }else{
+                    res.status(500).send('ERROR')
+                    throw err;
+                }
+            })
+        }
+    }catch(error){  
+        console.log(error);
+    }
+})
+
+
 
 module.exports = router;
 
